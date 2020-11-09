2e7b6eec0f1e696ab38d2b18ba52ba6174378bec
delasimensah <mensadelasi@gmail.com>
2020-11-09 16:30:25 +0000

    updates

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,9 +7,9 @@ const HomePage = () => {
   return (
     <Layout>
       <div className="min-h-screen w-[90vw] lg:w-[70vw] mx-auto grid grid-cols-1 lg:grid-cols-[2fr,1fr] gap-10">
-        <div className="h-screen border">posts</div>
+        <div className="">posts</div>
 
-        <div className="h-screen border">sidebar</div>
+        <div className="hidden lg:block">sidebar</div>
       </div>
     </Layout>
   );
