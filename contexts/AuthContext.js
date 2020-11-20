14dcb1be2161040e474f4209d23517b2e88e420f
delasimensah <mensadelasi@gmail.com>
2020-11-20 16:30:25 +0000

    connected to api

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -1,5 +1,6 @@
 import React, { createContext, useContext } from "react";
-import { auth, functions } from "../firebase/firebaseClient";
+import { auth } from "../firebase/firebaseClient";
+import axios from "axios";
 
 const AuthContext = createContext();
 
@@ -7,22 +8,40 @@ export const authState = () => {
   return useContext(AuthContext);
 };
 
+const devUrl =
+  "http://localhost:5001/social-media-93a8a/us-central1/api/createUser";
+
+const url =
+  "http://localhost:5001/social-media-93a8a/us-central1/api/createUser";
+
 export const AuthProvider = ({ children }) => {
   const signin = async (email, password) => {
     await auth.signInWithEmailAndPassword(email, password);
   };
 
   const signup = async (username, email, password) => {
-    await auth.createUserWithEmailAndPassword(email, password);
-    await auth.currentUser.updateProfile({
+    const { user } = await auth.createUserWithEmailAndPassword(email, password);
+
+    await user.updateProfile({
       displayName: username,
       photoURL:
         "https://firebasestorage.googleapis.com/v0/b/social-media-93a8a.appspot.com/o/nouser.jpg?alt=media&token=c57bec01-5543-4178-9a4a-8c188235996a",
     });
-    await functions.httpsCallable("createUser")({
-      username,
-      email,
-    });
+
+    const token = await user.getIdToken();
+
+    await axios.post(
+      url,
+      {
+        username,
+        email,
+      },
+      {
+        headers: {
+          Authorization: `${token}`,
+        },
+      }
+    );
   };
 
   const signout = async () => {
