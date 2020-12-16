8eb4b826e9f678c2f59b07b870bed484b4982c7b
delasimensah <mensadelasi@gmail.com>
2020-12-16 16:30:25 +0000

    added updates profile and cover images functionality

--- components/ProfileInfo.jsx
+++ components/ProfileInfo.jsx
@@ -1,23 +1,128 @@
-import React from "react";
+import React, { useState } from "react";
 import { format } from "timeago.js";
 import { withAuthUser, useAuthUser } from "next-firebase-auth";
-import { IoLocationOutline, IoCalendarOutline } from "react-icons/io5";
+import {
+  IoLocationOutline,
+  IoCalendarOutline,
+  IoCameraOutline,
+  IoCogOutline,
+} from "react-icons/io5";
+import { firestore, storage } from "../firebase/firebaseClient";
 
 //components
 import EditProfileButton from "./EditProfileButton";
 
 const ProfileInfo = ({ userInfo, posts }) => {
   const AuthUser = useAuthUser();
+  const [loading, setLoading] = useState(false);
+  const [loadingProfile, setLoadingProfile] = useState(false);
+
+  const updateCoverImage = async (e) => {
+    const file = e.target.files[0];
+    setLoading(true);
+
+    try {
+      const ref = storage.ref(
+        `${AuthUser.displayName}/${new Date().toISOString()}_${file.name}`
+      );
+
+      const snapshot = await ref.put(file);
+
+      const url = await snapshot.ref.getDownloadURL();
+
+      await firestore.doc(`users/${AuthUser.id}`).update({
+        coverPhoto: url,
+      });
+      setLoading(false);
+    } catch (error) {
+      console.log(error.message);
+      setLoading(false);
+    }
+  };
+
+  const updateProfileImage = async (e) => {
+    const file = e.target.files[0];
+    setLoadingProfile(true);
+
+    try {
+      const ref = storage.ref(
+        `${AuthUser.displayName}/${new Date().toISOString()}_${file.name}`
+      );
+
+      const snapshot = await ref.put(file);
+
+      const url = await snapshot.ref.getDownloadURL();
+
+      await AuthUser.firebaseUser.updateProfile({
+        photoURL: url,
+      });
+
+      await firestore.doc(`users/${AuthUser.id}`).update({
+        profileImage: url,
+      });
+
+      setLoadingProfile(false);
+    } catch (error) {
+      console.log(error.message);
+      setLoadingProfile(false);
+    }
+  };
 
   return (
     <div>
       <div className="relative">
-        <div className="h-40 overflow-hidden bg-gray-400 rounded-md md:h-60">
+        <div className="relative h-40 overflow-hidden bg-gray-400 rounded-md md:h-60">
           <img src={userInfo?.coverPhoto} alt="" className="w-full h-full" />
+
+          {AuthUser.id === userInfo.id && (
+            <label
+              htmlFor="cover"
+              className="absolute flex items-center p-2 text-gray-100 bg-gray-700 rounded-full cursor-pointer top-5 right-5 hover:bg-purple-600"
+            >
+              {loading ? (
+                <IoCogOutline className="w-5 h-5 animate-spin" />
+              ) : (
+                <IoCameraOutline className="w-5 h-5" />
+              )}
+              <input
+                type="file"
+                id="cover"
+                accept=".png,.jpeg,.jpg"
+                className="hidden"
+                onChange={updateCoverImage}
+              />
+            </label>
+          )}
         </div>
 
-        <div className="absolute w-24 h-24 overflow-hidden border-4 border-[#f4f4f4] dark:border-darklight rounded-full -bottom-10 md:w-32 md:h-32 lg:-bottom-14 lg:left-10 left-5 bg-gray-400">
-          <img src={userInfo?.profileImage} alt="" className="w-full h-full" />
+        <div className="absolute w-24 h-24 bg-transparent md:w-32 md:h-32 -bottom-10 lg:left-10 left-5 lg:-bottom-14 ">
+          <div className="relative w-full h-full border-4 border-[#f4f4f4] dark:border-darklight rounded-full   bg-gray-400">
+            <img
+              src={userInfo?.profileImage}
+              alt=""
+              className="w-full h-full rounded-full"
+            />
+
+            {AuthUser.id === userInfo.id && (
+              <label
+                htmlFor="profile"
+                className="absolute bottom-0 right-0 flex items-center p-2 text-gray-100 bg-gray-700 rounded-full cursor-pointer hover:bg-purple-600"
+              >
+                {loadingProfile ? (
+                  <IoCogOutline className="w-5 h-5 animate-spin" />
+                ) : (
+                  <IoCameraOutline className="w-5 h-5" />
+                )}
+                <input
+                  type="file"
+                  id="profile"
+                  accept=".png,.jpeg,.jpg"
+                  className="hidden"
+                  onChange={updateProfileImage}
+                />
+              </label>
+            )}
+          </div>
         </div>
       </div>
 
